023190c2b922c9e70ba8171a63444330bd731d9d
Austinet <austinetsplendour@gmail.com>
2023-10-05 04:05:47 +0100

    ch: updated files, pulled changes

---
+++ src/components/Mushrooms.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import avatar from "../assets/images/home/avatar-m.png";
+import eye from "../assets/images/home/eye.svg";
+
+const Mushrooms = () => {
+  return (
+    <section className=" bg-[url('/src/assets/images/home/mushroom-bg.png')] bg-no-repeat bg-cover bg-center">
+      <div className="lg:pt-[22rem]  pt-[7.5rem] pb-[2.5rem] lg:pb-[2.5rem] bg-gradient-to-b from-gradientBG to-[#A259FF]">
+        <div className="lg:w-10/12 w-11/12 mx-auto flex flex-col relative">
+          <div>
+            <p className="w-[9.744rem] px-[1.25rem] rounded-[1.25rem] py-[0.62rem] text-base text-white font-sans leading-[140%] flex items-center justify-center gap-[0.75rem]  bg-primaryBrown">
+              <img src={avatar} alt="Avatar" />
+              <span>Shroomie</span>
+            </p>
+            <h2 className="text-white text-[2.375rem] lg:text-[3.188rem] font-sans leading-[120%] my-[1.88rem] font-semibold">
+              Magic Mushrooms
+            </h2>
+          </div>
+          <div className="text-white bg-shadowBG p-[1.88rem] rounded-[1rem] md:absolute right-0 bottom-0">
+            <p className="text-[0.75rem] font-mono leading-[110%] mb-[0.62rem]">
+              Auction ends in:
+            </p>
+            <ul className="flex justify-between">
+              <li>
+                <h3 className="text-[2.375rem] font-bold font-mono leading-[120%]">
+                  59
+                </h3>
+                <p className="text-[0.75rem] font-mono leading-[110%]">Hours</p>
+              </li>
+              <li>
+                <h3 className="text-[2.375rem] font-bold font-mono leading-[120%]">
+                  :
+                </h3>
+              </li>
+              <li>
+                <h3 className="text-[2.375rem] font-bold font-mono leading-[120%]">
+                  59
+                </h3>
+                <p className="text-[0.75rem] font-mono leading-[110%]">
+                  Minutes
+                </p>
+              </li>
+              <li>
+                <h3 className="text-[2.375rem] font-bold font-mono leading-[120%]">
+                  :
+                </h3>
+              </li>
+              <li>
+                <h3 className="text-[2.375rem] font-bold font-mono leading-[120%]">
+                  59
+                </h3>
+                <p className="text-[0.75rem] font-mono leading-[110%]">
+                  Seconds
+                </p>
+              </li>
+            </ul>
+          </div>
+          <Link className="w-full md:w-[12.375rem] text-base font-semibold font-sans leading-[140%] flex items-center justify-center gap-[0.75rem] px-[1.38rem] md:px-[1.38rem] bg-white  lg:h-[3.75rem] rounded-[1.25rem] h-[3rem] my-[1.88rem] md:my-0">
+            <img src={eye} alt="Eye" />
+            <span>See NFT</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Mushrooms;
---
+++ src/components/TopCreators.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import { creators } from "../utils/contents";
+import rocketLaunch from "../assets/images/home/rocketLaunch.svg";
+
+const TopCreators = () => {
+  return (
+    <section className="bg-darkBrown  py-[2.5rem] xl:py-[5rem]">
+      <div className="lg:w-10/12 w-11/12 mx-auto relative">
+        <div className="text-white">
+          <h2 className="text-[1.75rem] xl:text-[2.375rem] font-sans font-semibold mb-[0.62rem] leading-[140%] xl:leading-[120%]">
+            Top Creators
+          </h2>
+          <p className="text-base text-[1.375rem] font-sans leading-[140%] xl:leading-[160%] max-w-[40rem]">
+            Checkout Top Rated Creators On The NFT Marketplace
+          </p>
+        </div>
+        <div className="grid xl:grid-cols-4  sm:grid-cols-2 grid-cols-1 gap-[1.25rem] md:gap-[1.88rem]  my-[2.5rem]  xl:mt-[3.75rem] xl:mb-0">
+          {creators.map((creator) => (
+            <Link key={creator.id} className={creator.display}>
+              <div className="relative  bg-[#3B3B3B] text-white p-[1.25rem] rounded-[1.25rem] flex xl:flex-col   items-center gap-[1.25rem] ">
+                <div className="relative w-1/3 xl:w-full">
+                  <p className="w-[1.875rem] h-[1.875rem] rounded-[1.25rem] bg-darkBrown font-mono text-base text-[#858584] flex items-center justify-center absolute left-[0.35rem] top-[0.45rem]">
+                    {creator.id}
+                  </p>
+                  <img
+                    src={creator.profilePicture}
+                    alt="Space"
+                    className="mx-auto"
+                  />
+                </div>
+                <div className="w-2/3 xl:w-full">
+                  <h3 className="xl:text-center font-sans text-[1.375rem] font-semibold leading-[140%] mb-[0.62rem]">
+                    {creator.username}
+                  </h3>
+                  <div className="flex items-center xl:justify-between gap-[0.62rem]">
+                    <h4 className="font-sans text-base text-[#858584] leading-[140%]">
+                      Total Stake:
+                    </h4>
+                    <p className="font-mono text-base text-white leading-[140%]">
+                      {creator.totalStake}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <div className="sm:absolute top-[0.75rem] xl:top-[1.05rem] right-0">
+          <Link className="text-white text-base font-semibold font-sans leading-[140%rem] flex items-center justify-center gap-[0.75rem]  border border-2 border-primaryPurple rounded-[1.25rem] md:w-[15.43rem] sm:w-[11rem]  w-full h-[3.75rem]">
+            <img src={rocketLaunch} alt="Rocket launch" />
+            <span>View Rankings</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopCreators;
